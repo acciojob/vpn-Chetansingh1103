8a61de534ce6f8840b216f82c3f1063e6651edcd
Chetan Singh <boyzingsing@gmail.com>
2023-03-04 19:23:32 +0530

    fifth

--- src/main/java/com/driver/controllers/AdminController.java
+++ src/main/java/com/driver/controllers/AdminController.java
@@ -41,7 +41,7 @@ public class AdminController {
             throw new NullPointerException();
         }
         catch (Exception e){
-            throw new Exception("Country not found");
+            throw new Exception();
         }
     }
 }
